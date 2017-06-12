8f589398db6c496c625dc487599169f871fc8052
Ravi Kanodia <ravileonkanodia@gmail.com>
2017-06-11 23:31:40 -0700

    :construction: Adds non-functional status display

--- js/TicTacToeBoard.js
+++ js/TicTacToeBoard.js
@@ -14,6 +14,29 @@ class TicTacToeSquare extends React.Component {
   }
 }
 
+class TicTacToeStatus extends React.Component {
+  render() {
+    return React.createElement(
+      'div',
+      {
+        className: 'tic-tac-toe-status',
+      },
+      React.createElement(
+        'div',
+        {
+          className: 'tic-tac-toe-state',
+        },
+        this.props.stateMessage),
+      React.createElement(
+        'div',
+        {
+          className: 'tic-tac-toe-spectators',
+        },
+        this.props.spectatorsMessage)
+    );
+  }
+}
+
 class TicTacToeBoard extends React.Component {
   constructor() {
     super();
@@ -23,23 +46,43 @@ class TicTacToeBoard extends React.Component {
     };
   }
 
+  getStateMessage() {
+    return "Next player: " + this.state.nextPlayer;
+  }
+
+  getSpectatorsMessage() {
+    return "No spectators";
+  }
+
   render() {
     return React.createElement(
       'div',
       {
-        className: 'tic-tac-toe-board',
+        className: 'tic-tac-toe',
       },
-      _.map(this.state.squares, (value, index) =>
+      React.createElement(
+        'div',
         {
+          className: 'tic-tac-toe-board'
+        },
+        _.map(this.state.squares, (value, index) => {
           return React.createElement(
-            TicTacToeSquare,
-            {
-              key: index,
-              value: this.state.squares[index],
-              onclick: () => console.log('click ' + index)
-            }
-          );
-        }));
+              TicTacToeSquare,
+              {
+                key: index,
+                value: this.state.squares[index],
+                onclick: () => console.log('click ' + index)
+              }
+            );
+        })
+      ),
+      React.createElement(
+        TicTacToeStatus,
+        {
+          stateMessage: this.getStateMessage(),
+          spectatorsMessage: this.getSpectatorsMessage()
+        })
+    );
   }
 };
 
